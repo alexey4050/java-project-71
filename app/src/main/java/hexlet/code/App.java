17a251b9aceb38ec7523209c86403c20a62a1487
Alexey Kiselev <alexey4050@mail.ru>
2025-01-12 13:18:10 +0300

    Added classes App and Differ

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "gendiff",
+        description = "Compares two configuration files and shows a differences.")
+
+public class App implements Runnable {
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this @|fg(cyan) help |@message and exit.")
+    boolean usageHelpRequested;
+
+    @Override
+    public void run() {
+        if (versionInfoRequested) {
+            System.out.println("Version 1.0.0");
+            return;
+        }
+
+        if (usageHelpRequested) {
+            return;
+        }
+        System.out.println("Please provide two configuration files to compare");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).execute(args);
     }
 }
