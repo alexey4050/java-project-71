fd00d5d94ecd488460c2d76d0ef4ccc449a3b4d0
Alexey Kiselev <alexey4050@mail.ru>
2025-01-12 19:27:19 +0300

    Adedd arguments and options in App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,16 +3,25 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
 
 @Command(name = "gendiff",
         description = "Compares two configuration files and shows a differences.")
 
 public class App implements Runnable {
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish", paramLabel = "format")
+    String formatSelection;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     boolean versionInfoRequested;
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this @|fg(cyan) help |@message and exit.")
     boolean usageHelpRequested;
 
+    @Parameters(index = "0", description = "path to first file")
+    File filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    File filepath2;
     @Override
     public void run() {
         if (versionInfoRequested) {
