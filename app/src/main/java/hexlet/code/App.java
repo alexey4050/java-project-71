38a8d607b25a39edc5f4aa3f0fec56c270ef4e96
Alexey Kiselev <alexey4050@mail.ru>
2025-01-13 13:58:41 +0300

    Added create, parsing and reading JSON files

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,13 +4,13 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-
-import java.io.File;
+import java.util.HashMap;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff",
         description = "Compares two configuration files and shows a differences.")
 
-public class App implements Runnable {
+public class App implements Callable<Integer> {
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish", paramLabel = "format")
     String formatSelection;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
@@ -19,23 +19,45 @@ public class App implements Runnable {
     boolean usageHelpRequested;
 
     @Parameters(index = "0", description = "path to first file")
-    File filepath1;
+    String filepath1;
     @Parameters(index = "1", description = "path to second file")
-    File filepath2;
+    String filepath2;
     @Override
-    public void run() {
+    public Integer call() {
         if (versionInfoRequested) {
             System.out.println("Version 1.0.0");
-            return;
+            return 0;
         }
 
         if (usageHelpRequested) {
-            return;
+            return 0;
         }
-        System.out.println("Please provide two configuration files to compare");
+        if (filepath1 == null || filepath2 == null) {
+            System.out.println("Please provide two configuration files to compare");
+            return 1;
+        }
+        System.out.println("Files provided: " + filepath1 + ", " + filepath2);
+        return 0;
     }
 
     public static void main(String[] args) {
-        new CommandLine(new App()).execute(args);
+        try {
+            var dataToAddFilepath1 = new HashMap<String, Object>();
+            dataToAddFilepath1.put("host", "hexlet.io");
+            dataToAddFilepath1.put("timeout", 50);
+            dataToAddFilepath1.put("proxy", "123.234.53.22");
+            dataToAddFilepath1.put("follow", false);
+
+            var dataToAddFilepath2 = new HashMap<String, Object>();
+            dataToAddFilepath2.put("timeout", 20);
+            dataToAddFilepath2.put("verbose", true);
+            dataToAddFilepath2.put("host", "hexlet.io");
+            Differ.createFile("src/main/resources/filepath1.json", dataToAddFilepath1);
+            Differ.createFile("src/main/resources/filepath2.json", dataToAddFilepath2);
+
+            new CommandLine(new App()).execute(args);
+        } catch (Exception e) {
+            System.out.println("Ошибка " + e.getMessage());
+        }
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,32 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
 public class Differ {
-    public static String generate() {
-            return "Ошибка чтения файла";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void createFile(String filePath, Map<String, Object> content) throws Exception {
+        String jsonContent = objectMapper.writeValueAsString(content);
+        Path writefilePath = Paths.get(filePath);
+        Files.writeString(writefilePath, jsonContent);
+    }
+
+    public static Map<String, Object> parseJsonFile(String filePath) throws Exception {
+        Path path = Paths.get(filePath).toAbsolutePath().normalize();
+
+        if (!Files.exists(path)) {
+            throw new Exception("File '" + path + "' does not exist.");
+        }
+        return objectMapper.readValue(Files.readString(path), Map.class);
+    }
+
+    public static String generate(String filepath1, String filepath2) throws Exception {
+        Map<String, Object> config1 = parseJsonFile(filepath1);
+        Map<String, Object> config2 = parseJsonFile(filepath2);
+        return "comparing files: " + config1 + " and " + config2;
     }
 }
