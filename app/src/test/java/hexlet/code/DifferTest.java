392dc0290cc5177e8429e28f6af5857b21f97208
Alexey Kiselev <alexey4050@mail.ru>
2025-01-17 20:50:28 +0300

    Added method testGenerateYaml in DifferTest class

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,10 +3,12 @@ package hexlet.code;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("A special test case")
@@ -26,7 +28,7 @@ class DifferTest {
     public void tetsCreateFile() throws Exception {
         Map<String, Object> content = new HashMap<>();
         content.put("key", "value");
-        Differ.createFile(FILEPATH1, content);
+        Parser.createJsonFile(FILEPATH1, content);
 
         String jsonContent = new String(Files.readAllBytes(Paths.get(FILEPATH1)));
         String expected = "{\"key\":\"value\"}";
@@ -37,9 +39,9 @@ class DifferTest {
     public void testParseJsonFile() throws Exception {
         Map<String, Object> content = new HashMap<>();
         content.put("key", "value");
-        Differ.createFile(FILEPATH1, content);
+        Parser.createJsonFile(FILEPATH1, content);
 
-        Map<String, Object> parsedContent = Differ.parseJsonFile(FILEPATH1);
+        Map<String, Object> parsedContent = Parser.parseJsonFile(FILEPATH1);
         assertEquals(content, parsedContent);
     }
 
@@ -50,8 +52,8 @@ class DifferTest {
         Map<String, Object> content2 = new HashMap<>();
         content2.put("key", "value");
 
-        Differ.createFile(FILEPATH1, content1);
-        Differ.createFile(FILEPATH2, content2);
+        Parser.createJsonFile(FILEPATH1, content1);
+        Parser.createJsonFile(FILEPATH2, content2);
 
         String result = Differ.generate(FILEPATH1, FILEPATH2);
         String expected = "{\n    key: value\n}";
@@ -65,8 +67,8 @@ class DifferTest {
         Map<String, Object> content2 = new HashMap<>();
         content2.put("key", "value2");
 
-        Differ.createFile(FILEPATH1, content1);
-        Differ.createFile(FILEPATH2, content2);
+        Parser.createJsonFile(FILEPATH1, content1);
+        Parser.createJsonFile(FILEPATH2, content2);
 
         String result = Differ.generate(FILEPATH1, FILEPATH2);
         String expected = "{\n  - key: value1\n  + key: value2\n}";
@@ -80,8 +82,8 @@ class DifferTest {
         Map<String, Object> content2 = new HashMap<>();
         content2.put("key2", "value2");
 
-        Differ.createFile(FILEPATH1, content1);
-        Differ.createFile(FILEPATH2, content2);
+        Parser.createJsonFile(FILEPATH1, content1);
+        Parser.createJsonFile(FILEPATH2, content2);
 
         String result = Differ.generate(FILEPATH1, FILEPATH2);
         String expected = "{\n  - key1: value1\n  + key2: value2\n}";
@@ -93,8 +95,8 @@ class DifferTest {
         Map<String, Object> content1 = new HashMap<>();
         Map<String, Object> content2 = new HashMap<>();
 
-        Differ.createFile(FILEPATH1, content1);
-        Differ.createFile(FILEPATH2, content2);
+        Parser.createJsonFile(FILEPATH1, content1);
+        Parser.createJsonFile(FILEPATH2, content2);
 
         String result = Differ.generate(FILEPATH1, FILEPATH2);
         String expected = "{\n}";
@@ -110,8 +112,8 @@ class DifferTest {
         content2.put("key2", "value2");
         content2.put("key3", "value3");
 
-        Differ.createFile(FILEPATH1, content1);
-        Differ.createFile(FILEPATH2, content2);
+        Parser.createJsonFile(FILEPATH1, content1);
+        Parser.createJsonFile(FILEPATH2, content2);
 
         String result = Differ.generate(FILEPATH1, FILEPATH2);
         String expected = "{\n  - key1: value1\n    key2: value2\n  + key3: value3\n}";
@@ -125,11 +127,40 @@ class DifferTest {
         Map<String, Object> content2 = new HashMap<>();
         content2.put("key", false);
 
-        Differ.createFile(FILEPATH1, content1);
-        Differ.createFile(FILEPATH2, content2);
+        Parser.createJsonFile(FILEPATH1, content1);
+        Parser.createJsonFile(FILEPATH2, content2);
 
         String result = Differ.generate(FILEPATH1, FILEPATH2);
         String expected = "{\n  - key: true\n  + key: false\n}";
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGenerateYaml() throws Exception {
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("host", "hexlet.io");
+        data1.put("timeout", 50);
+        data1.put("proxy", "123.234.53.22");
+        data1.put("follow", false);
+
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("timeout", 20);
+        data2.put("verbose", true);
+        data2.put("host", "hexlet.io");
+
+        Parser.createYamlFile("src/test/resources/filePath1.yml", data1);
+        Parser.createYamlFile("src/test/resources/filePath2.yml", data2);
+
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+        String actual = Differ.generate("src/test/resources/filePath1.yml", "src/test/resources/filePath2.yml");
+        assertEquals(expected.trim(), actual.trim());
+
     }
 }
