b0ed1cefddb53a33a028959180c22b97b3b43262
Alexey Kiselev <alexey4050@mail.ru>
2025-01-21 21:30:10 +0300

    Added tests, in directory resources file json and plain_expected.txt

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -14,10 +14,19 @@ class DifferTest {
     @Test
     void testYamlStylish() throws Exception {
         String expectedStylish = Files.readString(Paths.get(fixturesPath
-                + "stylish_expected.txt")); // Читаем ожидаемый результат из файла
+                + "stylish_expected.txt"));
         String actualStylish = Differ.generate(fixturesPath
                 + "file1.yml", fixturesPath + "file2.yml", "stylish");
         assertEquals(expectedStylish.trim(), actualStylish.trim());
+    }
+
+    @Test
+    void testPlainFormatter() throws Exception {
+        String expectedPlain = Files.readString(Paths.get(fixturesPath
+                + "plain_expected.txt"));
+        String actualPlain = Differ.generate(fixturesPath
+                + "file1.json", fixturesPath + "file2.json", "plain");
+        assertEquals(expectedPlain.trim(), actualPlain.trim());
     }
 }
 
