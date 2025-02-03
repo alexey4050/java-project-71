86c1b4db5ae04d146e94b55179609c2efe2326c3
Alexey Kiselev <alexey4050@mail.ru>
2025-02-03 13:22:39 +0300

    The tests in the class DifferTest have been corrected

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,19 +1,23 @@
 package hexlet.code;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 class DifferTest {
-    private static String expectedPlain;
-    private static String expectedStylish;
-    private static String expectedJson;
+    private static String expectedPlainJson;
+    private static String expectedStylishJson;
+    private static String expectedJsonJson;
+    private static String expectedPlainYml;
+    private static String expectedStylishYml;
+    private static String expectedJsonYml;
+
 
     private static Path getFixturePath(String fileName) {
         return Paths.get("src", "test", "resources", "fixtures", fileName)
@@ -27,21 +31,48 @@ class DifferTest {
 
     @BeforeAll
     public static void beforeAll() throws Exception {
-        expectedJson = readFixture("json_expected.json");
-        expectedPlain = readFixture("plain_expected.txt");
-        expectedStylish = readFixture("stylish_expected.txt");
+        expectedJsonJson = readFixture("json_expected.json");
+        expectedPlainJson = readFixture("plain_expected_json.txt");
+        expectedStylishJson = readFixture("stylish_expected_json.txt");
+
+        expectedJsonYml = readFixture("json_expected.json");
+        expectedPlainYml = readFixture("plain_expected_yml.txt");
+        expectedStylishYml = readFixture("stylish_expected_yml.txt");
+    }
+
+    private void performTest(String format, String expected, String formatter) throws Exception {
+        String filePath1 = getFixturePath("file1." + format).toString();
+        String filePath2 = getFixturePath("file2." + format).toString();
+        assertThat(Differ.generate(filePath1, filePath2, formatter)).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yml"})
-    public void generateTest(String format) throws Exception {
-        String filePath1 = getFixturePath("file1." + format).toString();
-        String filePath2 = getFixturePath("file2." + format).toString();
+    void generateTestForStylishFormatter(String format) throws Exception {
+        String expectedStylish = format.equals("json") ? expectedStylishJson : expectedStylishYml;
+        performTest(format, expectedStylish, "stylish");
+    }
 
-        assertThat(Differ.generate(filePath1, filePath2)).isEqualTo(expectedStylish);
-        assertThat(Differ.generate(filePath1, filePath2, "stylish")).isEqualTo(expectedStylish);
-        assertThat(Differ.generate(filePath1, filePath2, "plain")).isEqualTo(expectedPlain);
-        assertThat(Differ.generate(filePath1, filePath2, "json")).isEqualTo(expectedJson);
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    void generateTestForPlainFormatter(String format) throws Exception {
+        String expectedStylish = format.equals("json") ? expectedPlainJson : expectedPlainYml;
+        performTest(format, expectedStylish, "plain");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    void generateTestForJsonFormatter(String format) throws Exception {
+        String expectedStylish = format.equals("json") ? expectedJsonJson : expectedJsonYml;
+        performTest(format, expectedStylish, "json");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void generateTestWithoutExplicitFormatter(String format) throws Exception {
+        String expectedStylish = format.equals("json") ? expectedStylishJson : expectedStylishYml;
+        assertThat(Differ.generate(getFixturePath("file1." + format).toString(),
+                getFixturePath("file2." + format).toString())).isEqualTo(expectedStylish);
     }
 }
 
